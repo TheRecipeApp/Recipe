eb157d162c79c525f641562287bf728cfc2349c6
Alex Doan <alexdoan@gmail.com>
2017-10-18 23:11:55 -0700

    Committing current state of branch

--- RecipeApp/ViewControllers/Main/ExploreViewController.swift
+++ RecipeApp/ViewControllers/Main/ExploreViewController.swift
@@ -8,11 +8,26 @@
 
 import UIKit
 
-class ExploreViewController: UIViewController {
-
+class ExploreViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let recipeCell = tableView.dequeueReusableCell(withIdentifier: "DiscoverRecipeCell") as! DiscoverRecipeCell
+        return recipeCell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
         // Do any additional setup after loading the view.
     }
 
